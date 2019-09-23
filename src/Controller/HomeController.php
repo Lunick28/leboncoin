db8f8d4f1d8bcc57f9c7af243d0cea77f91035b9
Killian <roussetkillian1583@gmail.com>
2019-09-23 15:55:46 +0200

    Mise en place CSS (inscription et connexion)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,11 +11,29 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="home")
      **/
     public function index() {
         return $this->render('index.html.twig', [
             'title' => 'Le Bon Coin'
+        ]);
+    }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     **/
+    public function inscription() {
+        return $this->render('inscription.html.twig', [
+            'title' => 'Inscription'
+        ]);
+    }
+
+    /**
+     * @Route("/connexion", name="connexion")
+     **/
+    public function connexion() {
+        return $this->render('connexion.html.twig', [
+            'title' => 'Connexion'
         ]);
     }
 }
